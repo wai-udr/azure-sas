e61dc9a54321cdf04d5cc8285d8a31264d168657
udr <u.dreher@waelli.ch>
2025-02-16 08:27:05 +0100

    Code cleanup

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,19 +2,18 @@ import express, { Request, Response } from 'express';
 import { BlobSASPermissions, generateBlobSASQueryParameters, StorageSharedKeyCredential } from '@azure/storage-blob';
 const router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req: Request, res: Response, next: Function) {
-  const storageAccountName = "STORAGE_ACCOUNT_NAME";
-  const storageAccountKey = "STORAGE_ACCOUNT_KEY"; // Sicher speichern, z. B. in Azure Key Vault
+  const storageAccountName = 'STORAGE_ACCOUNT_NAME';
+  const storageAccountKey = 'STORAGE_ACCOUNT_KEY'; // Sicher speichern, z. B. in Azure Key Vault
     
   // SAS-Token für den Client generieren
   const expiry = new Date();
   expiry.setHours(expiry.getHours() + 1); // 1 Stunde gültig
 
   const sasOptions = {
-    containerName: "mein-container",
-    blobName: "mein-blob",
-    permissions: BlobSASPermissions.parse("r"), // Nur Lesezugriff
+    containerName: 'mein-container',
+    blobName: 'mein-blob',
+    permissions: BlobSASPermissions.parse('r'), // Nur Lesezugriff
     startsOn: new Date(),
     expiresOn: expiry
   };
